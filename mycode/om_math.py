9110c7c7373d3ddb6c897833014b418e39db11a0
nbgitpuller <nbgitpuller@nbgitpuller.link>
2024-12-01 23:29:59 +0000

    Automatic commit by nbgitpuller

--- mycode/om_math.py
+++ mycode/om_math.py
@@ -6,23 +6,23 @@ from numpy import log10
 # C4.8  Adding Incoherent Sound Levels
 def incoherent_sum_log10(spl):
     # pass #Remove once you add code
-
+ 
     # C4.8.0  Determine the Number of SPLs
     num_levels = len(spl) 
     
     # C4.8.1  Check to see whether  `spl` is Iterable
     if num_levels < 2:
         return spl
-    
+    else    
     # C4.8.2  Initialize the Incoherent Sum
-    sum_incoherent = 0
-    
+        sum_incoherent = 0
+
     # C4.8.3  Iterate Through the List    
-    for l in spl:               
-        sum_incoherent += 10 ** (l/10)
-        
+        for l in spl:               
+            sum_incoherent += 10 ** (l/10)
+
     # C4.8.3  Map it Back to Decibels
-    return 10 * np.log10(sum_incoherent)
+        return 10 * np.log10(sum_incoherent)
         
         
     
